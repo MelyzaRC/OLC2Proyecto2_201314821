2f675eb8ab93dd56dc6ebd3422b87206d544e528
MelyzaRC <mlzdrg792@gmail.com>
2023-05-01 23:47:45 -0600

    Correccion en funcion printf

--- Proyecto1/Instruction/func_printf.cpp
+++ Proyecto1/Instruction/func_printf.cpp
@@ -25,7 +25,7 @@ void func_printf::traducir(environment *env, asttree *tree, generator_code *gen)
             tree->erroresSemanticos++;
 
 
-            //concatenar false
+            //convierto el nulo a string para que se pueda imprimir
             std::string tmpNulo = gen->newTemp();
             gen->AddAssign(tmpNulo, "H");
             gen->AddComment("N");
@@ -45,81 +45,33 @@ void func_printf::traducir(environment *env, asttree *tree, generator_code *gen)
             result.TipoExpresion = STRING;
             result.Value = tmpNulo;
         }
-            /*if(result.TipoExpresion == INTEGER)
-            {
-                gen->AddComment("************imprimiendo numero************");
-                gen->AddPrintf("d", "(int)"+ result.Value);
-                gen->AddPrintf("c", "10");
-                gen->AddBr();
-            }else if(result.TipoExpresion == FLOAT)
-            {
-                gen->AddComment("************imprimiendo numero************");
-                gen->AddPrintf("f", "(float)"+ result.Value);
-                gen->AddPrintf("c", "10");
-                gen->AddBr();
-            }
-            else*/ if(result.TipoExpresion == STRING)
-            {
-                gen->AddComment("************imprimiendo string************");
-                //llamar a generar printstring
-                gen->GeneratePrintString();
-                //agregar codigo en el main
-                std::string NewTemp1 = gen->newTemp();
-                std::string NewTemp2 = gen->newTemp();
-                int size = env->Size;
-                gen->AddComment("nuevo temporal en pos vacia");
-                gen->AddExpression(NewTemp1, "P", std::to_string(size), "+");
-                gen->AddComment("se deja espacio de retorno");
-                gen->AddExpression(NewTemp1, NewTemp1, "1", "+");
-                gen->AddComment("se coloca string en parametro que se manda");
-                gen->AddSetStack("(int)"+ NewTemp1, result.Value);
-                gen->AddComment("cambio de entorno");
-                gen->AddExpression("P", "P", std::to_string(size), "+");
-                gen->AddComment("Llamada");
-                gen->AddCall("olc3d_printString");
-                gen->AddComment("obtencion retorno");
-                gen->AddGetStack( "(int)P",NewTemp2);
-                gen->AddComment("regreso del entorno");
-                gen->AddExpression("P", "P", std::to_string(size), "-");
-                gen->AddComment("salto de linea");
-                gen->AddPrintf("c", "10");
-                gen->AddBr();
-            }/*
-            else if(result.TipoExpresion == BOOL)
-            {
-                gen->AddComment("************imprimiendo bool************");
-                std::string newLabel = gen->newLabel();
-                std::string newFalseLabel = gen->newLabel();
-                if(result.IsTemp)
-                {
-                    gen->AddIf(result.Value, "1", "!=", newFalseLabel);
-                }
-                //add true labels
-                for(int i=0; i < result.TrueLvl.size(); i++)
-                {
-                    gen->AddLabel(result.TrueLvl[i]);
-                }
-                gen->AddPrintf("c", "(char)116");
-                gen->AddPrintf("c", "(char)114");
-                gen->AddPrintf("c", "(char)117");
-                gen->AddPrintf("c", "(char)101");
-                gen->AddGoto(newLabel);
-                //add false labels
-                for(int j=0; j < result.FalseLvl.size(); j++)
-                {
-                    gen->AddLabel(result.FalseLvl[j]);
-                }
-                gen->AddLabel(newFalseLabel);
-                gen->AddPrintf("c", "(char)102");
-                gen->AddPrintf("c", "(char)97");
-                gen->AddPrintf("c", "(char)108");
-                gen->AddPrintf("c", "(char)115");
-                gen->AddPrintf("c", "(char)101");
-                gen->AddLabel(newLabel);
-                gen->AddPrintf("c", "10");
-                gen->AddBr();
-            }*/
+        if(result.TipoExpresion == STRING){
+            gen->AddComment("************imprimiendo string************");
+            //llamar a generar printstring
+            gen->GeneratePrintString();
+            //agregar codigo en el main
+            std::string NewTemp1 = gen->newTemp();
+            std::string NewTemp2 = gen->newTemp();
+            int size = env->Size;
+            gen->AddComment("nuevo temporal en pos vacia");
+            gen->AddExpression(NewTemp1, "P", std::to_string(size), "+");
+            gen->AddComment("se deja espacio de retorno");
+            gen->AddExpression(NewTemp1, NewTemp1, "1", "+");
+            gen->AddComment("se coloca string en parametro que se manda");
+            gen->AddSetStack("(int)"+ NewTemp1, result.Value);
+            gen->AddComment("cambio de entorno");
+            gen->AddExpression("P", "P", std::to_string(size), "+");
+            gen->AddComment("Llamada");
+            gen->AddCall("olc3d_printString");
+            gen->AddComment("obtencion retorno");
+            gen->AddGetStack( "(int)P",NewTemp2);
+            gen->AddComment("regreso del entorno");
+            gen->AddExpression("P", "P", std::to_string(size), "-");
+            gen->AddBr();
+        }
     }
+    gen->AddComment("salto de linea");
+    gen->AddPrintf("c", "10");
 }
 
 void func_printf::ejecutar(environment *env, asttree *tree){
